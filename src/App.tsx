5a52deb17968dd784beeb9345c52df551f7c2700
Demian Licht <demianlicht@hotmail.com>
2022-12-04 20:59:11 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,22 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>Save the Date</h1>
+    <p>¡Gracias por ser parte de un día tan especial en nuestras vidas!</p>
+   <h2>Gift Giving</h2>
+   <p>Verlos a todos ese día es el mejor regalo. Sin embargo, si desean obsequiarnos un regalo de bodas adicional, agradecemos que sea una contribución monetaria ya vivimos en el extranjero.</p>
+  <p>Utiliza este link: </p>
+  <p>para poder realizar tu contribución si deseas. ¡Estaremos increíblemente agradecidos!</p>
+  <h3> Allergies and Others</h3>
+  <p>Por favor dinos con anticipación si tienes alergia a algún alimento o si NO tomas alcohol.</p>
+  <h4>This is an UNPLUGGED wedding!</h4>
+  <p>Para nosotros es muy importante disfrutar del momento, por lo que pedimos que en la ceremonia, entrada de la novia y el First Dance, ¡NO utilizemos celulares! Queremos que vivas el momento con nosotros y no a través de tu lente.</p>
+  <p>PS: Tendremos muchos fotógrafos y compartiremos todas las fotos con ustedes.</p>
+  <h5>Y el paso más importante, ¡tu RSVP!</h5>
+  <p>¡Por favor haznos saber si contamos contigo ese día!</p>
+  <p>See you at the Altar!</p>
+  </> 
   );
 }
 
